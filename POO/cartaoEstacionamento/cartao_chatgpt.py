1e85d774c280cce0be13f9a8cdd110f7afd6d996
Lucas Batista <batistalucasdev@gmail.com>
2024-11-28 00:28:14 -0300

    2024.11.28

--- POO/cartaoEstacionamento/cartao_chatgpt.py
+++ POO/cartaoEstacionamento/cartao_chatgpt.py
@@ -27,65 +27,53 @@ Especificações:
 - Crie pelo menos três objetos da classe, representando cartões de estacionamento diferentes.
 - Demonstre os métodos implementados e suas validações em ação.
 '''
-
 from datetime import datetime
-import random
-import string
 
-class CartaoEstacionamento:
-    def __init__(self):
-        self.__numero_cartao = self.gerar_numero_cartao()
-        self.__data_hora_entrada = datetime.now()
-        self.__status = "Ativo"
-        self.__data_hora_saida = None
-        self.__valor_total = 0.0
-
-    def gerar_numero_cartao(self):
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-
+class cartaoEstacionamento():
+    
+    def __init__(self, numero_cartao, data_hora_entrada, status_cartao, data_hora_saida, valor_total):
+        self.__numero_cartao = numero_cartao #atributo imutavel
+        self.__data_hora_entrada = self.valida_data_hora(data_hora_entrada) #atributo imutavel
+        self.__status_cartao = 'Ativo' #atributo mutavel
+        self.__data_hora_saida = self.valida_data_hora(data_hora_saida) #atributo imutavel, registra quando o cartao é finalizado
+        self.__valor_total = valor_total #atributo imutavel 
+        
     @property
-    def numero_cartao(self):
+    def numero_cartao (self):
         return self.__numero_cartao
-
+        
     @property
     def data_hora_entrada(self):
         return self.__data_hora_entrada
 
+    def valida_data_hora (self, data):
+        try:
+            data = datetime.strptime(data, "%d/%m/%Y")
+            return data
+        except ValueError:
+            raise ValueError("Data de entrada inválida")
+    
+    def valida_hora (self, hora):
+        try:
+            hora = datetime.strptime(hora, "%H:%M")
+            return hora
+        except ValueError:
+            raise ValueError("Hora de entrada inválida")
+    
     @property
-    def status(self):
-        return self.__status
-
-    @property
-    def data_hora_saida(self):
-        return self.__data_hora_saida
-
+    def status_cartao (self):
+        return self.__status_cartao
+    
     @property
     def valor_total(self):
         return self.__valor_total
 
-    def registrar_saida(self, data_hora_saida):
+    def registrar_saida(self):
         if self.__status == "Finalizado":
             print("O cartão já foi finalizado.")
             return
-
-        formatos_aceitos = ["%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M"]
-
-        for formato in formatos_aceitos:
-            try:
-                data_hora_saida = datetime.strptime(data_hora_saida, formato)
-                break
-            except ValueError:
-                continue
-        else:
-            print("Data/hora de saída inválida. Use o formato: YYYY-MM-DD HH:MM ou YYYY-MM-DD HH:MM:SS")
-            return
-
-        if data_hora_saida < self.__data_hora_entrada:
-            print("A data/hora de saída não pode ser anterior à data/hora de entrada.")
-            return
-
-        self.__data_hora_saida = data_hora_saida
-
+        
+        self.__data_hora_saida = datetime.now()
         tempo_permanencia = self.__data_hora_saida - self.__data_hora_entrada
         horas = tempo_permanencia.total_seconds() / 3600
 
@@ -93,30 +81,24 @@ class CartaoEstacionamento:
             self.__valor_total = 8.0
         else:
             self.__valor_total = 8.0 + ((horas - 2) * 4)
-
+        
         self.__status = "Finalizado"
         print(f"Saída registrada. Valor total: R$ {self.__valor_total:.2f}")
-
-    def __str__(self):
-        entrada_formatada = self.__data_hora_entrada.strftime("%d/%m/%Y %H:%M:%S")
-        saida_formatada = self.__data_hora_saida.strftime("%d/%m/%Y %H:%M:%S") if self.__data_hora_saida else "Ainda não registrada"
-        saida01 = f'Número do cartão: {self.__numero_cartao}\nData/Hora de Entrada: {entrada_formatada}'
-        saida02 = f'Status: {self.__status}\nData/Hora de Saída: {saida_formatada}'
-        saida03 = f'Valor Total: R$ {self.__valor_total:.2f}'
-        return f'{saida01}\n{saida02}\n{saida03}'
-
-cartao1 = CartaoEstacionamento()
-print(cartao1)
-cartao1.registrar_saida("2024-11-28 10:30")
-print(cartao1)
-print("")
-cartao2 = CartaoEstacionamento()
-print(cartao2)
-cartao2.registrar_saida("2024-11-30 22:47")
-print(cartao2)
-print("")
-cartao3 = CartaoEstacionamento()
-print(cartao3)
-cartao3.registrar_saida("2024-12-14 09:21")
-print(cartao3)
-print("")
+     
+    @property
+    def data_hora_saida(self):
+        return self.__data_hora_saida
+    
+    def valida_data (self, data):
+        try:
+            data = datetime.strptime(data, "%d/%m/%Y")
+            return data
+        except ValueError:
+            raise ValueError("Data de saída inválida")
+    
+    def valida_hora (self, hora):
+        try:
+            hora = datetime.strptime(hora, "%H:%M")
+            return hora
+        except ValueError:
+            raise ValueError("Hora de saída inválida")
