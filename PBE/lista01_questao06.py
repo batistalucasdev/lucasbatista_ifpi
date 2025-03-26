c064dfae59c4d3192b5a4077e839a520b1478adc
Lucas Batista <batistalucasdev@gmail.com>
2025-03-26 20:54:46 -0300

    2025.03.26

--- PBE/lista01_questao06.py
+++ PBE/lista01_questao06.py
@@ -3,26 +3,28 @@
 - Se o número de lados for igual a 3, escrever TRIÂNGULO e o valor do seu perímetro.
 - Se o número de lados for igual a 4, escrever QUADRADO e o valor da sua área.
 - Se o número de lados for igual a 5, escrever PENTÁGONO.
+Observação: Considere que o usuário só informará os valores 3, 4 ou 5.
 Obs.: Sempre tratar a entrada de dados para evitar que o programa aborte.
 '''
 
 def verifica_lados(numero_lados, medida_lado):
     perimetro = numero_lados * medida_lado
     if numero_lados == 3:
-        print(f'TRIÂNGULO; Valor do perímetro = {perimetro:.2f}.')
+        return print(f'TRIÂNGULO; Valor do perímetro = {perimetro:.2f}.')
     elif numero_lados == 4:
-        print(f'QUADRADO; Valor do perímetro = {perimetro:.2f}.')
+        return print(f'QUADRADO; Valor do perímetro = {perimetro:.2f}.')
     elif numero_lados == 5:
-        print(f'PENTÁGONO; Valor do perímetro = {perimetro:.2f}.')
-    return 
+        return print(f'PENTÁGONO; Valor do perímetro = {perimetro:.2f}.')
     
 while True:
     try:
         numero_lados = int(input("\nDigite o número de lados de um polígono regular: "))
-        medida_lado = int(input("\nDigite a medida do lado deste polígono: "))
-
-        lados = verifica_lados(numero_lados, medida_lado)
         
+        if numero_lados == 3 or numero_lados == 4 or numero_lados == 5:
+            medida_lado = float(input("\nDigite a medida do lado deste polígono: "))
+            lados = verifica_lados(numero_lados, medida_lado)
+        else:
+            print(f'\nValor inválido. Digite apenas 3, 4 ou 5.')
         break
     except:
-        print("\nNúmero inválido. Digite novamente!")
+        print("\nValor inválido. Digite novamente!")
---
+++ PBE/lista01_questao08.py
@@ -0,0 +1,25 @@
+'''
+8. Escreva uma função que lê um caractere digitado pelo usuário e retorna este caractere somente se ele for igual a 'S' ou 'N'. Se o caractere não for nem 'S' nem 'N', a função imprime a mensagem 'Caractere inválido. Digite novamente'. Use esta função em um programa que fica lendo do usuário um número qualquer e imprime este número ao cubo na tela. O programa deve ficar lendo os números até o usuário responder 'N' à pergunta se ele deseja continuar ou não.
+Obs.: Sempre tratar a entrada de dados para evitar que o programa aborte.
+'''
+
+def calcular_cubo(numero):
+    cubo = numero ** 3
+    return print(f"\n{numero} elevado ao cubo = {cubo}.")
+
+def continuar(resposta):
+    if resposta == 'S':
+        return resposta
+    elif resposta == 'N':
+        return resposta
+    else:
+        return print(f"\nResposta inválida. Digite apenas 'S' ou 'N'!")
+    
+while True:
+    try:
+        numero = int(input("\nDigite um número qualquer: "))
+        cubo = calcular_cubo(numero)
+        #pergunta = continuar(resposta)
+        break
+    except:
+        print("\nValor inválido. Digite novamente!")
